a4ed2df17acaac61c2ae1d50950de32e93b16e1e
elizadora <elizadoradasilva2003@gmail.com>
2021-10-01 23:46:21 -0300

    Estressados parte 1

--- Estressados/parte1/estressadosp1.cpp
+++ Estressados/parte1/estressadosp1.cpp
@@ -77,7 +77,7 @@ int procurar_menor_pos(std::vector<int> fila) {
    return min;
 }
 
-int procurar_menor_pos_apos(std::vector<int> fila, int pos) {
+int procurar_menor_pos_apartir(std::vector<int> fila, int pos) {
    int menorApos = pos;
 
    for (int i = pos; i < (int)fila.size(); i++) {
@@ -88,7 +88,7 @@ int procurar_menor_pos_apos(std::vector<int> fila, int pos) {
    return menorApos;
 }
 
-int procurar_melhor_se(std::vector<int> fila) {
+int procurar_melhor_pos_se(std::vector<int> fila) {
    int posHC = 0;
 
    for (int i = 0; i < (int)fila.size(); i++) {
@@ -157,7 +157,7 @@ std::string qual_metade_eh_mais_estressada(std::vector<int> fila) {
       return "segunda";
 }
 
-std::string homens_sao_mais_estressados_que_mulheres(std::vector<int> fila) {
+bool homens_sao_mais_estressados_que_mulheres(std::vector<int> fila) {
    int contH = 0;
    int contM = 0;
    float mediaHomem = 0;
@@ -177,16 +177,56 @@ std::string homens_sao_mais_estressados_que_mulheres(std::vector<int> fila) {
    mediaMulher = mediaMulher / contM;
    mediaHomem = mediaHomem / contH;
 
-   if (mediaHomem == mediaMulher)
-      return "a media de stress deu empate";
-   else if (mediaMulher > mediaHomem)
-      return "a media de stress das mulheres";
+   if (mediaHomem > mediaMulher)
+      return true;
    else
-      return "a media de stress dos homens";
+      return false;
 }
 
 int main() {
 
-   
+   //Busca
+
+   std::cout << existe({-9, - 8, 3, 2, -98, 1}, 1) << "\n"; //true
+   std::cout << existe({-9, - 8, 3, 2, -98, 1}, -1) << "\n"; //false
+
+   std::cout << contar({1, 2, -98, 4, 5, 9, 12, 2, -3, 2}, 2) << "\n"; //3
+   std::cout << contar({1, 2, -98, 4, 5, 9, 12, 2, -3}, 3) << "\n"; //0
+
+   std::cout << procurar_valor({1, 3, 45, -9, -12, -99, 2, -99}, -99) << "\n"; //5
+   std::cout << procurar_valor({1, 3, 45, -9, -12, -99, 2, -99}, -2) << "\n"; //-1
+
+   std::cout << procurar_valor_apos({1, 3, 45, -9, -12, -99, 2, -99}, -99, 6) << "\n"; //7
+   std::cout << procurar_valor_apos({1, 3, 45, -9, -12, -99, 2, -99}, -99, 5) << "\n"; //5
+   std::cout << procurar_valor_apos({1, 3, 45, -9, -12, -99, 2, -99}, 2, 5) << "\n"; //6
+
+
+   //Melhor Caso
+
+   std::cout << procurar_menor({1, -55, 12, 99, 87, -2, 7}) << "\n"; //-55
+
+   std::cout << procurar_maior({1, -55, 12, 99, 87, -2, 7}) << "\n"; //99
+
+   std::cout << procurar_menor_pos({1, -55, 12, 99, 87, -2, 7}) << "\n"; //1
+
+   std::cout << procurar_menor_pos_apartir({1, -55, 12, 99, 87, -2, 7}, 3) << "\n"; //5
+
+   std::cout << procurar_melhor_pos_se({5, -55, 12, 99, 87, 2, 7}) << "\n"; //5
+
+   //Contagem 
+
+   std::cout << calcular_stress_medio({5, -55, 10, 14, -20}) << "\n"; //20.8
+
+   std::cout << mais_homens_ou_mulheres({5, -55, 10, 14, -20}) << "\n"; //homens
+   std::cout << mais_homens_ou_mulheres({-55, 10, 14, -20}) << "\n"; //empate
+   std::cout << mais_homens_ou_mulheres({5, -55, -10, 14, -20}) << "\n"; //mulheres
+
+   std::cout << qual_metade_eh_mais_estressada({60, 3, -4, 9, 12, 33}) << "\n"; //primeira
+   std::cout << qual_metade_eh_mais_estressada({66, 1, 2, 99, 5, 2}) << "\n"; //segunda
+   std::cout << qual_metade_eh_mais_estressada({60, 2, 3, 5, 59, 1}) << "\n"; //empate
+
+   std::cout << homens_sao_mais_estressados_que_mulheres({-5, -55, 70, 14}) << "\n"; //true
+   std::cout << homens_sao_mais_estressados_que_mulheres({-55, 10, 14, -20}) << "\n";  //false
+
    return 0;
 }
