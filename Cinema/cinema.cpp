78620d835767348d5581ecad09361f50ccee0239
elizadora <elizadoradasilva2003@gmail.com>
2021-11-17 10:37:04 -0300

    Atualizacao cinema

--- Cinema/cinema.cpp
+++ Cinema/cinema.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
+#include <memory>
 
 class Client{
-
     private:
         std::string id;
         std::string fone;
@@ -37,63 +37,58 @@ class Client{
 
 class Sala{
     private:
-        std::vector<Client> cadeiras;
+        std::vector<std::shared_ptr<Client>> cadeiras;
 
-    public:
-        Sala(int capacidade = 0){
-            if(capacidade > 0){
-                for(int i = 0; i < capacidade; i++){
-                    Client cli;
-                    this->cadeiras.push_back(cli);
+        int procurarPessoa(std::string id){
+            for(int i = 0; i < (int) cadeiras.size(); i++){
+                if(cadeiras[i] != nullptr && cadeiras[i]->getId() == id){
+                    return i;
                 }
             }
+
+            return -1;
         }
+
+    public:
+        Sala(int capacidade = 0): cadeiras(capacidade, nullptr){}
 
         int getCadeiras() const{
             return this->cadeiras.size();
         }
 
         friend std::ostream &operator<<(std::ostream &os, const Sala &sala){
-            if(sala.cadeiras.empty()){
-                os << "[ ]\n";
-            }else{
-                os << "[ ";
+           os << "[ ";
 
-                for(int i = 0; i < sala.getCadeiras(); i++){
-                    if(sala.cadeiras[i].getId() == ""){
-                        os << "- ";
-                    }else{
-                        os << sala.cadeiras[i] << " ";
-                    }
-                }
+           for(auto cadeira : sala.cadeiras){
+               if(cadeira == nullptr){
+                   os << "- ";
+               }else{
+                   os << *cadeira << " ";
+               }
+           }
 
-                os << "]\n";                
-
-            }
+            os << "]\n";
 
             return os;
         }
 
         bool reservar(std::string id, std::string fone, int lug){
-            if(lug < 0 || lug > this->getCadeiras() || this->getCadeiras() == 0){
+            if(lug < 0 || lug > this->getCadeiras()){
                 std::cout << "Lugar invalido\n";
                 return false;
             }
 
-            if(this->cadeiras[lug].getId() != ""){
+            if(this->cadeiras[lug] != nullptr){
                 std::cout << "Lugar ocupado\n";
                 return false;
             }
 
-            for(int i = 0; i < this->getCadeiras(); i++){
-                if(this->cadeiras[i].getId() == id){
-                    std::cout << "Pessoa ja esta presente na sala\n";
-                    return false;
-                }
+            if(procurarPessoa(id) != -1){
+                std::cout << "Pessoa ja esta presente na sala\n";
+                return false;
             }
 
-            Client cli(id, fone);
-            this->cadeiras[lug] = cli;
+            this->cadeiras[lug] = std::make_shared<Client>(id, fone);
             return true;
         }
 
@@ -103,12 +98,9 @@ class Sala{
             }else{
                 bool result{false};
 
-                for(int i = 0; i < this->getCadeiras(); i++){
-                    if(this->cadeiras[i].getId() == id){
-                        Client cli;
-                        this->cadeiras[i] = cli;
-                        result = true;
-                    }
+                if(procurarPessoa(id) != -1){
+                    this->cadeiras[procurarPessoa(id)] = nullptr;
+                    result = true;
                 }
 
                 if(!result) std::cout << "Id invalido\n";
