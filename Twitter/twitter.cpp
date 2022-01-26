a0794719832767efdbb4eec734383b20031749f4
elizadora <elizadoradasilva2003@gmail.com>
2022-01-26 18:30:08 -0300

    Twitter update 2

--- Twitter/twitter.cpp
+++ Twitter/twitter.cpp
@@ -17,24 +17,27 @@ public:
     }
 };
 
-class Message{
+class Tweet{
+private:
     int id;
     std::string username;
     std::string msg;
     std::set<std::string> likes;
+    Tweet* rt {nullptr};
+    bool deleted{false};
 
 public:
-    Message(int id, std::string username, std::string msg) : id{id}, username{username}, msg{msg}{}
+    Tweet(int id, std::string username, std::string msg) : id{id}, username{username}, msg{msg}{}
 
-    friend std::ostream &operator<<(std::ostream& os, const Message& message){
-        os << message.getId() << ":" << message.username << " (" << message.msg << ")";
-        if((int) message.likes.size() > 0){
+    friend std::ostream &operator<<(std::ostream& os, const Tweet& tweet){
+        os << tweet.getId() << ":" << tweet.username << " (" << tweet.msg << ")";
+        if((int) tweet.likes.size() > 0){
             os << "  " << "[";
 
             int size = 1;
 
-            for(auto user : message.likes){
-                if(size < (int) message.likes.size()){
+            for(auto user : tweet.likes){
+                if(size < (int) tweet.likes.size()){
                     os << user << ", ";
 
                 }else{
@@ -45,6 +48,10 @@ public:
             }
 
             os << "]";
+        }
+
+        if(tweet.rt != nullptr){
+            os << "\n\t" << *tweet.rt;
         }
 
         return os; 
@@ -58,58 +65,77 @@ public:
         return this->id;
     }
 
+    std::string getSender() const{
+        return this->username;
+    }
+
+    std::string getMsg() const{
+        return this->msg;
+    }
+
+    std::set<std::string> getLikes() const{
+        return this->likes;
+    }
+
+    void setRt(Tweet* rt){
+        this->rt = rt;
+    }
+
+    void setDeleted(){
+        this->deleted = true;
+        this->msg = "tweet deletado";
+        this->username = "";
+        this->likes.clear();
+    }
+
+    bool isDeleted() const{
+        return this->deleted;
+    }
+
 };
 
 class Inbox{
-    std::map<int, Message*> unread;
-    std::map<int, Message*> allMsg;
+private:
+    std::map<int, Tweet*> timeline;
+    std::map<int, Tweet*> myTweets;
 
 public:
     Inbox(){}
 
-    friend std::ostream &operator<<(std::ostream& os, Inbox& inbox){
-        for(auto msg : inbox.getAll()){
+    friend std::ostream &operator<<(std::ostream& os, const Inbox& inbox){
+        for(auto msg : inbox.getTimeline()){
             os << *msg << "\n";
         }
 
         return os;
     }
 
-    void storeUnread(Message* tweet){
-        this->allMsg[tweet->getId()] = tweet;
-        this->unread[tweet->getId()] = tweet;
-    }
+    std::vector<Tweet*> getTimeline() const{
+        std::vector<Tweet*> temp;
 
-    void storeReaded(Message* tweet){
-        this->allMsg[tweet->getId()] = tweet;
-    }
-
-    std::vector<Message*> getAll(){
-        std::vector<Message*> temp;
-
-        for(auto message : this->allMsg){
-            temp.push_back(message.second);
+        for(auto it = this->timeline.rbegin(); it != this->timeline.rend(); ++it){
+            if(it->second->isDeleted() == false){
+                temp.push_back(it->second);
+            }
         }
 
         return temp;
     }
 
-    std::vector<Message*> getUnread(){
-        std::vector<Message*> temp;
+    std::vector<Tweet*> getMyTweets() const{
+        std::vector<Tweet*> temp;
 
-        for(auto message : this->unread){
-            temp.push_back(message.second);
+        for(auto it = this->myTweets.rbegin(); it != this->myTweets.rend(); ++it){
+            temp.push_back(it->second);
         }
-
-        this->unread.clear();
 
         return temp;
     }
 
-    Message* getTweet(int id){
-        auto it = allMsg.find(id);
+    Tweet* getTweet(int id) const{
+        auto it = timeline.find(id);
 
-        if(it != allMsg.end()){ 
+        if(it != timeline.end()){ 
             return it->second;
         
         }else{
@@ -117,10 +143,28 @@ public:
         }
     }
 
+    void storeInTimeline(Tweet* tweet){
+        this->timeline[tweet->getId()] = tweet;
+    }
+
+    void storeInMyTweets(Tweet* tweet){
+        this->myTweets[tweet->getId()] = tweet;
+        this->timeline[tweet->getId()] = tweet;
+    }
+
+    void rmMsgFrom(const std::string username){
+        for(auto tweet : this->timeline){
+            if(tweet.second->getSender() == username){
+                this->timeline.erase(tweet.second->getId());
+            }
+        }
+    }
+
 };
 
 
 class User{
+private:
     std::string username;
     std::map<std::string, User*> followers;
     std::map<std::string, User*> following;
@@ -130,7 +174,7 @@ public:
 
     User(std::string id) : username{id} {}
 
-    friend std::ostream &operator<<(std::ostream& os, User& user){
+    friend std::ostream &operator<<(std::ostream& os, const User& user){
         os << user.username << "\n";
         os << "\tseguidos\t[";
         int size = 1;
@@ -170,6 +214,29 @@ public:
         other->followers[this->username] = this;
     }
 
+    void unfollow(User* other){
+        this->inbox.rmMsgFrom(other->username);
+
+        this->following.erase(other->username);
+        other->followers.erase(this->username);
+    }
+
+    void unfollowAll(){
+        for(auto user : this->following){
+            user.second->followers.erase(this->username);
+        }
+
+        this->following.clear();
+    }
+
+    void rejectAll(){
+        for(auto user : this->followers){
+            user.second->following.erase(this->username);
+        }
+
+        this->followers.clear();
+    }
+
     Inbox* getInbox(){
         return &this->inbox;
     }
@@ -178,30 +245,34 @@ public:
         inbox.getTweet(tweetId)->like(this->username);
     }
 
-    void sendTweet(Message* tw){
-       inbox.storeReaded(tw);
+    void sendTweet(Tweet* tw){
+       inbox.storeInMyTweets(tw);
+       inbox.storeInTimeline(tw);
 
         for(auto foll : this->followers){
-            foll.second->getInbox()->storeUnread(tw);
+            foll.second->getInbox()->storeInTimeline(tw);
         }
     }
 
-    void unfollow(User* other){
-        this->following.erase(other->username);
-        other->followers.erase(this->username);
-    }
 };
 
 class Controller{
+private:
     int newTwID{0};
-    std::map<int, std::shared_ptr<Message>> tweets;
+    std::map<int, std::shared_ptr<Tweet>> tweets;
     std::map<std::string, std::shared_ptr<User>> users;
+
+    Tweet* createTweet(std::string username, std::string msg){
+        auto tweet = std::make_shared<Tweet>(newTwID++, username, msg);
+        this->tweets[tweet->getId()] = tweet;
+        return tweet.get();
+    }
 
 public:
 
     Controller() {}
     
-    friend std::ostream &operator<<(std::ostream& os, Controller& controller){
+    friend std::ostream &operator<<(std::ostream& os, const Controller& controller){
         for(auto user : controller.users){
             os << *user.second;
         }
@@ -218,6 +289,18 @@ public:
         }
     }
 
+    void rmUser(std::string username){
+        User* user = this->getUser(username);
+        user->unfollowAll();
+        user->rejectAll();
+
+        for(auto tweet : user->getInbox()->getMyTweets()){
+            tweet->setDeleted();
+        }
+
+        this->users.erase(username);
+    }
+
     User* getUser(std::string username){
         if(users.find(username) != users.end()){
             return this->users[username].get();
@@ -229,8 +312,15 @@ public:
     }
 
     void sendTweet(std::string username, std::string msg){
-        getUser(username)->sendTweet(new Message(newTwID, username, msg));
-        newTwID++;
+        getUser(username)->sendTweet(createTweet(username, msg));
+    }
+
+    void sendRt(std::string username, int id, std::string msg){
+        Tweet* rt = getUser(username)->getInbox()->getTweet(id);
+        Tweet* tw = createTweet(username, msg);
+
+        tw->setRt(rt);
+        getUser(username)->sendTweet(tw);
     }
 
 };
@@ -243,7 +333,7 @@ int main(){
         std::string linha{""};
         std::string comando{""};
 
-        std::cout << "$";
+        // std::cout << "$";
 
         getline(std::cin, linha);
 
@@ -253,14 +343,21 @@ int main(){
 
         try{
 
-            if(comando == "addUser"){
+            if(comando == "add"){
                 std::string username;
 
                 ss >> username;
 
                 controller.addUser(username);
             
-            }else if(comando == "show"){
+            }else if(comando == "rm"){
+                std::string username;
+
+                ss >> username;
+
+                controller.rmUser(username);
+            }
+            else if(comando == "show"){
                 std::cout << controller;
             
             }else if(comando == "follow"){
@@ -281,6 +378,17 @@ int main(){
 
                 controller.sendTweet(username, msg.substr(1));
             
+            }else if(comando == "rt"){
+                std::string username;
+                int id;
+                std::string msg;
+
+                ss >> username >> id;
+
+                getline(ss, msg);
+
+                controller.sendRt(username, id, msg.substr(1));
+
             }else if(comando == "timeline"){
                 std::string username;
 
@@ -304,14 +412,6 @@ int main(){
 
                 controller.getUser(user)->unfollow(controller.getUser(other));
             
-            }else if(comando == "unread"){
-                std::string username;
-
-                ss >> username;
-                for(auto msg : controller.getUser(username)->getInbox()->getUnread()){
-                    std::cout << *msg << "\n";
-                }
-            
             }else if(comando == "end"){
                 break;
             
@@ -323,4 +423,6 @@ int main(){
             std::cout << e.what() << '\n';
         }
     }
+
+    return 0;
 }
